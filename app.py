11e7efbb0259c8ad4caa7c62263e983ec25d78d0
jeanne <novasquared@yahoo.com>
2021-09-08 18:07:48 -0500

    added form logic to questions.html and updated route for /

--- app.py
+++ app.py
@@ -7,3 +7,8 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
 
 debug = DebugToolbarExtension(app)
+
+@app.get("/")
+def display_form():
+    questions = silly_story.prompts
+    return render_template("questions.html", prompts=questions)
