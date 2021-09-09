f72400a22f52d86241093d98f71af5fc44947f67
kimberly lin <kimberly.lin10@gmail.com>
2021-09-08 17:12:22 -0700

    implemented /result

--- app.py
+++ app.py
@@ -10,5 +10,15 @@ debug = DebugToolbarExtension(app)
 
 @app.get("/")
 def display_form():
-    questions = silly_story.prompts
-    return render_template("questions.html", prompts=questions)
+    """Generate and populate form with the list of prompts stories need"""
+    prompts = silly_story.prompts
+    return render_template("questions.html", prompts = prompts)
+
+# After form submission from "/", get input, save them
+# Redirect to /results which displays what silly_stories.generate should output
+
+@app.get("/result")
+def generate_story():
+    """Move to /result directory, display generated story based on form input"""
+    results = silly_story.generate(request.args)
+    return render_template("story.html", generated_story = results)
